6433a3d8fcddc0e2910e1107fe64c7cfdc546277
diadji <diadjindiaye24@gmail.com>
2020-05-06 04:50:09 +0000

    config deployment

--- config.py
+++ config.py
@@ -4,7 +4,11 @@ SECRET_KEY = "#d#JCqTTW\nilK\\7m\x0bp#\tj~#H"
 
 FB_APP_ID = 568636240738783
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+# Database initialization
+if os.environ.get('DATABASE_URL') is None:
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
